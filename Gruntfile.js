34e9aabc53346fda16a649092ed0cf05fbbf5fd5
Paul Wood <paulwooddesign@gmail.com>
2014-11-19 16:14:46 -0700

    Major Updates Across the board

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,7 +123,8 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '10.1.10.51',
+        // hostname: 'localhost',
         livereload: 12345
       },
       livereload: {
